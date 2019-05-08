3bdc253425f7151d0e9f7031e2ba69e79de73d1e
agdod <50316115+agdod@users.noreply.github.com>
2019-05-08 07:39:47 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # static responsive gallery preloaed with 4 demo images
 # changed background to darkgrey
 # moved CCS to seperate stylesheet
+
+basic genaertion of gallery using php - trail run 
+opens directory and takes each image in directory and displays it.
+needs major refining
--- gallery.php
+++ gallery.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html>
-<body>
+
 <?php
-$dir = "/gallery"
+$dir = "gallery/";
 
 //open directory and read contents
 if (is_dir($dir)){
     if ($dh = opendir($dir)){
         while (($file = readdir($dh)) !== false){
+            $imagepath=$dir.$file;
             echo "<div class='column'>";
-            echo "<img src=" . $file . "style='width:100%' onclick='openModal();currentSlide(1)' class='hover-shadow cursor'>";
+            echo "<img src=$imagepath style='width:100%' onclick='openModal();currentSlide(1)' class='hover-shadow cursor'>";
             echo "</div>";
         }
         closedir($dh);
         }
     }
-}
 ?>
-</body>
-</html>
