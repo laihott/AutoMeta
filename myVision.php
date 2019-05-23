16ebf0789319c0b8a6cad6fa4012e99767a71a19
agdod <adodson1510@hotmail.com>
2019-05-23 14:25:37 +0300

    request_Response Functioning

--- myVision.php
+++ myVision.php
@@ -129,7 +129,7 @@ function getLabelsResults( $response ) {
     } 
 }   /* End of get label results */
 
-function getFacesResults() {
+function getFacesResults($response) {
 
     /**************************************************************************
      * Faceail Detection supports mulitple faces wihtin an image along wiht the 
@@ -137,7 +137,68 @@ function getFacesResults() {
      * FACIL RECOGNITON IS NOT SUPPORTED
      * *********************************************************************** */
 
+    $faces = $response ->getFaceAnnotations();
+    if ($faces){
+        $likeliHood = ["Unkown", "Very Unlikely", "Unlikely", "Possible", "Likely", "Very Likely"];
+        echo ( count($faces). " : faces detected <br/>");
+        foreach ($faces as $face) {
+            $anger = $face->getAngerLikelihood();
+            printf("Anger: " . "<BR/>" .  $likeliHood[$anger]);
+            $joy = $face->getJoyLikelihood();
+            printf("Joy: " . "<BR/>" .  $likeliHood[$joy]);
+            $surprise = $face->getSurpriseLikelihood();
+            printf("Surprise: " . "<BR/>" .  $likeliHood[$surprise]);
+        // get bounds
+            $vertices = $face->getBoundingPoly()->getVertices();
+            $bounds = [];
+            foreach ($vertices as $vertex) {
+                $bounds[] = sprintf((""), $vertex->getX(), $vertex->getY());
+            }
+            print('Bounds: ' . join(', ',$bounds) . PHP_EOL);
+        print(PHP_EOL);
+        }
+    } 
 }
+        /***************************************************
+         * 
+         *      Draw box arounf faces in progress 
+         *      requires GD extension
+         * 
+         *      # draw box around faces
+         *      if ($faces && $outFile) {
+         *          $imageCreateFunc = [
+         *          'png' => 'imagecreatefrompng',
+         *           'gd' => 'imagecreatefromgd',
+         *           'gif' => 'imagecreatefromgif',
+         *           'jpg' => 'imagecreatefromjpeg',
+         *           'jpeg' => 'imagecreatefromjpeg',
+         *           ];
+         *       $imageWriteFunc = [
+         *           'png' => 'imagepng',
+         *           'gd' => 'imagegd',
+         *           'gif' => 'imagegif',
+         *           'jpg' => 'imagejpeg',
+         *           'jpeg' => 'imagejpeg',
+         *        ];
+         *      copy($path, $outFile);
+         *      $ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+         *      if (!array_key_exists($ext, $imageCreateFunc)) {    
+         *               throw new \Exception('Unsupported image extension');
+         *      }
+         *      $outputImage = call_user_func($imageCreateFunc[$ext], $outFile);
+         *     foreach ($faces as $face) {
+         *         $vertices = $face->getBoundingPoly()->getVertices();
+         *           if ($vertices) {
+         *               $x1 = $vertices[0]->getX();
+         *               $y1 = $vertices[0]->getY();
+         *               $x2 = $vertices[2]->getX();
+         *               $y2 = $vertices[2]->getY();
+         *               imagerectangle($outputImage, $x1, $y1, $x2, $y2, 0x00ff00);
+         *           }
+         *       }
+         *       call_user_func($imageWriteFunc[$ext], $outputImage, $outFile);
+         *       printf('Output image written to %s' . "<BR/>" .  $outFile);
+         ***************************************************************************/
 
 function getLandmarksResults ( $response ) {
 
