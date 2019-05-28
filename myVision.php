74868454c8d2c69180ec7097cc04fded4a4af34b
agdod <adodson1510@hotmail.com>
2019-05-28 12:34:53 +0300

    -

--- myVision.php
+++ myVision.php
@@ -26,7 +26,7 @@ require __DIR__ . '/vendor/autoload.php';
 # imports the Google Cloud client library
 
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-use Google\Cloud\Vision\V1\Feature;
+//use Google\Cloud\Vision\V1\Feature;
 use Google\Cloud\Vision\V1\Feature\Type;
 
 function collectImgSource () {
@@ -134,8 +134,8 @@ $imgSource = "https://images.pexels.com/photos/257540/pexels-photo-257540.jpeg";
 /****    tempory disabled  *********
 //$requestedFeatures = collectFeature();  // Gather features image is to be analysed for 
 ************************************/
-$requestedFeatures = collectFeature();
-//$requestedFeatures = [TYPE::LABEL_DETECTION, TYPE::FACE_DETECTION, TYPE::LANDMARK_DETECTION, TYPE::OBJECT_LOCALIZATION, TYPE::SAFE_SEARCH_DETECTION] ;
+//$requestedFeatures = collectFeature();
+$requestedFeatures = [TYPE::LABEL_DETECTION, TYPE::FACE_DETECTION, TYPE::LANDMARK_DETECTION, TYPE::OBJECT_LOCALIZATION, TYPE::SAFE_SEARCH_DETECTION] ;
 
 
 $response = $imageAnnotator->annotateImage($imgSource,$requestedFeatures);
@@ -143,17 +143,44 @@ $response = $imageAnnotator->annotateImage($imgSource,$requestedFeatures);
 //$xmlFile = generate_XmlFileName($imgSource);
 
 // create the xml document
-//$xmlDoc = new DOMDocument('1.0', 'UTF-8');
-
+$xml = new DOMDocument('1.0', 'UTF-8');
 // create the root elemenet
-$root = $xmlDoc->appendChild($xmlDoc->createElement("ImageItem"));
+$root = $xml->createElement("ImageItem");
+$xml_fileInfo = $xml->createElement("Image");
+//split filename into approate parts -in progess
+//
+//
+//
+$xml_fileInfo->setAttribute("fileName","ImageFileName");
+$xml_fileInfo->setAttribute("location","FileImageLocation(local/web/GS");
 
-//create the Image element
-$imgTag = $root->appendChild($xmlDoc->createElement("Image"));
-//create the filename attribute
-$imgTag->appendChild($xmlDoc->createAttribute("filename"))->appendChild($xmlDoc->createTextNode($xmlFile));
-/** <Image filename="filename"> */
-$imgTag->appendChild($xmlDoc->createElement("Location",$loc));
+//function getSafeSearchResults( $response ){
+
+$likeliHood = ["Unkown", "Very Unlikely", "Unlikely", "Possible", "Likely", "Very Likely"];
+$safe = $response ->getSafeSearchAnnotation();
+$adult = $safe->getAdult();
+$spoof = $safe->getSpoof();
+$medical = $safe->getMedical();
+$violence = $safe->getViolence();
+$racy = $safe->getRacy();
+//generate XML safe Search Branch
+$xml_safeSearch = $xml->createElement("Safe_Search");
+$xml_safeSearch->setAttribute("Adult",$adult);
+$xml_safeSearch->setAttribute("Spoof",$spoof);
+$xml_safeSearch->setAttribute("Medical",$medical);
+$xml_safeSearch->setAttribute("Violence",$violence);
+$xml_safeSearch->setAttribute("Racy",$racy);
+$xml_fileInfo->appendChild($xml_safeSearch);
+/** echo ("Safe Search Results : <BR/>");
+*echo ("Adult : ".$likeliHood[$adult]."<BR/>");
+*echo ("Spoof : ".$likeliHood[$spoof]."<BR/>");
+*echo ("Medical : ".$likeliHood[$medical]."<BR/>");
+*echo ("Violence : ".$likeliHood[$violence]."<BR/>");
+*echo ("Racy : ".$likeliHood[$racy]."<BR/>"); */
+
+/******************************************************************** */
+/**************end of Safe Search Results *****************************/
+/******************************************************************** */
 
     /** ********** performs label detection on the image *************** *
      * write output to .xml file
@@ -165,29 +192,98 @@ $imgTag->appendChild($xmlDoc->createElement("Location",$loc));
      *      </LabelItem>
      *  </Labels>
      * ******************************************************************/   
-    $labels = $response->getLabelAnnotations();
+$labels = $response->getLabelAnnotations();
     if ($labels) {
-        echo("Labels:" ."<BR/>");
+        // Create Label branch 
+        $xml_label = $xml->createElement("Labels");
+        //echo("Labels:" ."<BR/>");
         foreach ($labels as $label) {
-            echo ($Label."<BR/>");
+            //echo ($Label."<BR/>");
             $prob = $label->getscore();
             $prob = round(($prob *= 100),2,PHP_ROUND_HALF_UP);
             $keyword = $label->getDescription();
-            //$imgTag->appendChild($xmlDoc->createElement("Description",$keyword));
-            //$imgTag->appendchild($xmlDoc->createAttribute("accuracy"))->appendChild($xmlDoc->createTextNode($prob));
-            //$xmlDoc ->formatOutput = true;
-            //echo $xmlDoc->saveXML();
+            // creat sub branch within Label Branch
+            $xml_labelItem = $xml->createElement("labelItem",$keyword);
+            $xml_labelItem->setAttribute("accuracy",$prob); // assign atribute to branch
+            $xml_label->appendChild($xml_labelItem); // write current Label sub branch to main 'Label' perant branch
+            //repeat for all label item
         }
     } else {
         echo("No Labels Detected <br/>");
 
-    }  /* End of get label results */
-    getSafeSearchResults($response,$xmlFile);
-    getFacesResults($response,$xmlFile);
-    getLandmarksResults($response,$xmlFile);
-    getObjectsResults($response,$xmlFile);
+    }  
+/********************************************************************************** */
+/************************** End of get label results ********************************/
+/********************************************************************************** */
+
+//function getLandmarksResults ( $response ) {
+
+$landmarks = $response->getLandmarkAnnotations();
+    //echo( "Landmark/s found :". count($landmarks)."<BR/>");
+    if ($landmarks) {
+        // Create Landmark Branch
+        $xml_landmark = $xml->createElement("Landmarks");
+        foreach ($landmarks as $landmark) {
+            $keyword = $landmark->getDescription();
+            $xml_landmarkItem = $xml->createElement("landmarkItem",$keyword);
+            $prob = $landmark->getscore();
+            $prob = round(($prob *= 100),2,PHP_ROUND_HALF_UP);
+            $xml_landmarkItem->setAttribute("accuracy",$prob);
+            $xml_landmark->appendChild($xml_landmarkItem);
+            //echo ($landmark->getDescription()."<BR/>");
+        } 
+    } else {
+        echo (" no landmarks detected");
+    }
+
+/************************************************************************************ */
+/********************* End of get Landmark Results **********************************/
+/************************************************************************************ */
+
+
+/****** function getObjectsResults( $response ) {*/
+
+    /**********************************
+     *  "localalizedObjectAnnotations": 
+     *      "name"
+     *      "score"
+     *      "boundingPoly" : {
+     *          "normalizedVertices":[
+     *              {x,y}, {x,y}, {x,y}, {x,y}] }
+     * 
+     ************************************/
+
+     $objects = $response->getLocalizedObjectAnnotations();
+     if ($objects) {
+        $xml_object = $xml ->createElement("Objects");
+        // echo(count($objects)." \"objects\" detected in Image");
+        foreach ($objects as $object) {
+            $name = $object->getName();
+            $prob = $object->getScore();
+            $prob = round(($prob *= 100),2,PHP_ROUND_HALF_UP);
+            $xml_objectItem = $xml->createElement("objectItem",$name);
+            $xml_objectItem->setAttribute("accuracy",$prob);
+            $vertices = $object->getBoundingPoly()->getNormalizedVertices();
+            $xml_objectVertices = $xml->createElement("vertices");
+            //echo("Confidence : ". $name.",".$score."<BR/>");
+            //print('Normalized bounding polygon vertices :'."<BR/>");
+            foreach ($vertices as $vertex){
+                $vertexX = $vertex->getX();
+                $vertexY = $vertex->getY();
+                $xml_objectVertices->setAttribute("verticesXY",$vertexxX.",".$vertexY);
+                $xml_objectItem->appendChild($xml_objectVertices);
+                //echo(" : $vertexX , $vertexY <BR/>");
+            }
+            $xml_object-> appendChild($xml_objectItem);    
+        }
+        //print(PHP_EOL);
+     } else {
+         echo (" No objects Detected");
+     }
+/************************* End of get object  results function **************************/
     
-function getFacesResults($response) {
+/*************************  function getFacesResults($response) { ***********************/
+
 
     /**************************************************************************
      * Faceail Detection supports mulitple faces wihtin an image along wiht the 
@@ -195,28 +291,57 @@ function getFacesResults($response) {
      * FACIL RECOGNITON IS NOT SUPPORTED
      * *********************************************************************** */
 
-    $faces = $response ->getFaceAnnotations();
-    if ($faces){
-        $likeliHood = ["Unkown", "Very Unlikely", "Unlikely", "Possible", "Likely", "Very Likely"];
-        echo ( count($faces). " : faces detected <br/>");
-        foreach ($faces as $face) {
-            $anger = $face->getAngerLikelihood();
-            printf("Anger: " . "<BR/>" .  $likeliHood[$anger]);
-            $joy = $face->getJoyLikelihood();
-            printf("Joy: " . "<BR/>" .  $likeliHood[$joy]);
-            $surprise = $face->getSurpriseLikelihood();
-            printf("Surprise: " . "<BR/>" .  $likeliHood[$surprise]);
-        // get bounds
-            $vertices = $face->getBoundingPoly()->getVertices();
-            $bounds = [];
-            foreach ($vertices as $vertex) {
-                $bounds[] = sprintf((""), $vertex->getX(), $vertex->getY());
-            }
-            print('Bounds: ' . join(', ',$bounds) . PHP_EOL);
-        print(PHP_EOL);
+$faces = $response ->getFaceAnnotations();
+if ($faces){
+    $xml_face = $xml->createElement("faces");
+
+    $likeliHood = ["Unkown", "Very Unlikely", "Unlikely", "Possible", "Likely", "Very Likely"];
+    //echo ( count($faces). " : faces detected <br/>");
+    foreach ($faces as $face) {
+        $xml_faceObject = $xml->createElement("faceObject");
+        $xml_faceEmotionProb = $xml->createElement("emotionalLikeliHood");
+        $anger = $face->getAngerLikelihood();
+        $xml_faceEmotion = $xml->createElement("emotion","Anger");
+        $xml_faceEmotion->setAttribute("Anger",$likeliHood[$anger]);
+        $xml_faceEmotionProb->appendChild($xml_faceEmotion);
+        //printf("Anger: " . "<BR/>" .  $likeliHood[$anger]);
+        $joy = $face->getJoyLikelihood();
+        $xml_faceEmotion = $xml->createElement("emotion","Joy");
+        $xml_faceEmotion->setAttribute("Joy",$likeliHood[$joy]);
+        $xml_faceEmotionProb->appendChild($xml_faceEmotion);
+        //printf("Joy: " . "<BR/>" .  $likeliHood[$joy]);
+        $surprise = $face->getSurpriseLikelihood();
+        $xml_faceEmotion = $xml->createElement("emotion","Surpise");
+        $xml_faceEmotion->setAttribute("Surpise",$likeliHood[$surpise]);
+        $xml_faceEmotionProb->appendChild($xml_faceEmotion);
+        //printf("Surprise: " . "<BR/>" .  $likeliHood[$surprise]);
+        $sorrow = $face->getSorrowLikelihood();
+        $xml_faceEmotion = $xml->createElement("emotion","Sorrow");
+        $xml_faceEmotion->setAttribute("Sorrow",$likeliHood[$sorrow]);
+        $xml_faceEmotionProb->appendChild($xml_faceEmotion);
+        //Output sorrow
+        $headwear = $face->getwearHeadwearLikelihood();
+        $xml_faceEmotion = $xml->createElement("emotion","Wearing headwear");
+        $xml_faceEmotion->setAttribute("Headwear",$likeliHood[$headwear]);
+        $xml_faceEmotionProb->appendChild($xml_faceEmotion);
+        //Output hedwearing
+        $xml_faceObject->appendChild($xml_faceEmotionProb);
+    // get bounds
+        $vertices = $face->getBoundingPoly()->getVertices();
+        $xml_faceVertices = $xml->createElement("vertices");
+        foreach ($vertices as $vertex) {
+            $vertexX = $vertex->getX();
+            $vertexY = $vertex->getY();
+            //$bounds[] = sprintf((""), $vertex->getX(), $vertex->getY());
+            $xml_faceVertices->setAttribute("verticesXY",$vertexxX.",".$vertexY);   
         }
-    } 
-} /**  End of Get Faces Results */
+        $xml_faceObject->appendChild($xml_faceVertices);
+        print('Bounds: ' . join(', ',$bounds) . PHP_EOL);
+    print(PHP_EOL);
+    }
+    $xml_face->appendChild($xml_faceObject);
+} 
+/**********************  End of Get Faces Results ********************************/
         /***************************************************
          * 
          *      Draw box arounf faces in progress 
@@ -258,70 +383,16 @@ function getFacesResults($response) {
          *       printf('Output image written to %s' . "<BR/>" .  $outFile);
          ***************************************************************************/
 
-function getLandmarksResults ( $response ) {
 
-    $landmarks = $response->getLandmarkAnnotations();
-    echo( "Landmark/s found :". count($landmarks)."<BR/>");
-    if ($landmarks) {
-        foreach ($landmarks as $landmark) {
-            echo ($landmark->getDescription()."<BR/>");
-        } 
-    } else {
-        echo (" no landmarks detected");
-    }
-} /* End of get Landmoark Results */
 
-function getObjectsResults( $response ) {
-
-    /**********************************
-     *  "localalizedObjectAnnotations": 
-     *      "name"
-     *      "score"
-     *      "boundingPoly" : {
-     *          "normalizedVertices":[
-     *              {x,y}, {x,y}, {x,y}, {x,y}] }
-     * 
-     ************************************/
-
-     $objects = $response->getLocalizedObjectAnnotations();
-     if ($objects) {
-         echo(count($objects)." \"objects\" detected in Image");
-        foreach ($objects as $object) {
-            $name = $object->getName();
-            $score = $object->getScore();
-            $vertices = $object->getBoundingPoly()->getNormalizedVertices();
-            echo("Confidence : ". $name.",".$score."<BR/>");
-            print('Normalized bounding polygon vertices :'."<BR/>");
-            foreach ($vertices as $vertex){
-                $vertexX = $vertex->getX();
-                $vertexY = $vertex->getY();
-                echo(" : $vertexX , $vertexY <BR/>");
-            }
-            
-        }
-        print(PHP_EOL);
-     } else {
-         echo (" No objects Detected");
-     }
-}   /* End of get object  results function */
-
-function getSafeSearchResults( $response ){
-
-    $likeliHood = ["Unkown", "Very Unlikely", "Unlikely", "Possible", "Likely", "Very Likely"];
-    $safe = $response ->getSafeSearchAnnotation();
-    $adult = $safe->getAdult();
-    $spoof = $safe->getSpoof();
-    $medical = $safe->getMedical();
-    $violence = $safe->getViolence();
-    $racy = $safe->getRacy();
-    echo ("Safe Search Results : <BR/>");
-    echo ("Adult : ".$likeliHood[$adult]."<BR/>");
-    echo ("Spoof : ".$likeliHood[$spoof]."<BR/>");
-    echo ("Medical : ".$likeliHood[$medical]."<BR/>");
-    echo ("Violence : ".$likeliHood[$violence]."<BR/>");
-    echo ("Racy : ".$likeliHood[$racy]."<BR/>");
-
-} /* end of Safe Search Results */
+        $xml_fileInfo->appendChild($xml_label);
+        $xml_fileInfo->appendChild($xml_landmark);
+        $xml_fileInfo->appendChild($xml_object);
+        $xml_fileInfo->appendchild($xml_face);
+        
+        $xml->appendChild($xml_fileInfo);
+        
+        echo $xml->saveXML();
     $imageAnnotator->close();
 
 
